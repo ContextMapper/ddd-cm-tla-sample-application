29118b1eacbbfcb22a57e0525e3af6620a644b8d
Stefan Kapferer <stefan.kapferer@ost.ch>
2023-08-31 11:18:11 +0200

    Ensure domain classes are jMoleculed

--- src/test/java/org/contextmapper/sample/tlas/domain/TacticDDDModelTest.java
+++ src/test/java/org/contextmapper/sample/tlas/domain/TacticDDDModelTest.java
@@ -17,11 +17,16 @@
 package org.contextmapper.sample.tlas.domain;
 
 import org.contextmapper.archunit.AbstractTacticArchUnitTest;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.core.domain.properties.HasType.Predicates.rawType;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 /**
  * This ArchUnit test uses our Context Mapper Extension to verify whether the code is in sync with the
  * Context Mapper domain model in TLAs-context.cml.
- *
+ * <p>
  * ArchUnit extension documentation: https://github.com/ContextMapper/context-mapper-archunit-extension
  */
 class TacticDDDModelTest extends AbstractTacticArchUnitTest {
@@ -40,4 +45,15 @@ class TacticDDDModelTest extends AbstractTacticArchUnitTest {
     protected String getJavaPackageName2Test() {
         return "org.contextmapper.sample.tlas.domain";
     }
+
+    @Test
+    void domainModelClassesShouldBeAnnotatedWithJMolecules() {
+        classes().that()
+                .areNotEnums()
+                .and().areNotAssignableTo(Exception.class)
+                .and().areNotNestedClasses()
+                .should().beAnnotatedWith(rawType(resideInAPackage("org.jmolecules..")))
+                .check(classes);
+    }
+
 }
